202170677425f0175af441decf79b3f72f321e05
Lulks <lucasfixina@gmail.com>
2022-11-19 14:59:09 -0400

    att

--- ex1.py
+++ ex1.py
@@ -1,19 +1,6 @@
-import math
-
-CatetoOposto = int(input('Digite o cateto oposto: '))
-CatetoAdjacente = int(input('Digite o cateto Adjacente: '))
-
-quadradocatetoo = CatetoOposto*CatetoOposto
-quadradocatetoadj = CatetoAdjacente*CatetoAdjacente
-
-hipotenusa = math.sqrt((quadradocatetoo + quadradocatetoadj))
-
-print('A hipotenusa do Cateto Oposto({}) e Cateto Adjacente({}) é : {} '.format(CatetoOposto,CatetoAdjacente,hipotenusa))
-
-
-
-
-
-#ceil pra cima 
-#floor pra baixo
-#trunc é o número inteiro
+import pygame
+pygame.init()
+pygame.mixer.music.load('E:\ESTUDOS\Phyton\msc01.wav')
+pygame.mixer.play()
+pygame.event.wait()
+print('...')
